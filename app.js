3c6e4df7b731ac9cc6216d168855d4d193c35d6a
asukatraloc22-rgb <asukatraloc22@gmail.com>
2026-06-29 19:39:06 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// app.js
+// Ce fichier gère l'interactivité de l'application.
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Magic Recipe est initialisé et prêt à cuisiner !");
+    // C'est ici que nous coderons la logique d'affichage à l'Étape 5.
+});
